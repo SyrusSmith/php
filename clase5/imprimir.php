3e40404d77ddd618a3b59d1796f92eb5b57b2e8f
SyrusSmith <nahueldibiase@hotmail.com>
2018-08-10 21:54:35 -0300

    Clase 5: archivos q faltaron

---
+++ clase5/imprimir.php
@@ -0,0 +1,20 @@
+<?php
+echo"<pre>";
+var_dump ($_POST);
+echo"</pre>";
+
+foreach ($_POST as $key => $value) {
+    echo "$key : $value";
+    echo "<br>";
+}
+
+echo "<br>";
+
+foreach ($_POST[hobbies] as $key=> $value) {
+    //hay que poner el [array] dentro de post para que el foreach lo recorra también
+    echo $key." ".$value;
+    echo "<br>";
+    
+}
+
+?>
